50bbda982295c3b53c41bb3ed5d99bd227b3489a
Alexander Wei <alexander.wei@tufts.edu>
2025-06-17 23:52:17 +0000

    Remove add to cart button. Cleanup

--- src/Public/ProductLoopWrapper.php
+++ src/Public/ProductLoopWrapper.php
@@ -16,13 +16,9 @@ class ProductLoopWrapper
 {
     public static function init(): void
     {
-        // Remove default WooCommerce product link open/close
-        // add_action('init', [__CLASS__, 'remove_default_product_link']);
 
         // Remove Add to Cart button from product loop after WooCommerce has registered its hooks
         add_action('init', [__CLASS__, 'remove_add_to_cart_from_loop'], 20);
-        // add_action('template_redirect', [__CLASS__, 'remove_add_to_cart_from_loop'], 20);
-
         // Add custom wrapper open/close
         add_action('woocommerce_before_shop_loop_item', [__CLASS__, 'custom_product_wrapper_open'], 10);
         add_action('woocommerce_after_shop_loop_item', [__CLASS__, 'custom_product_wrapper_close'], 5);
@@ -30,15 +26,7 @@ class ProductLoopWrapper
 
     public static function remove_add_to_cart_from_loop(): void
     {
-        error_log('QuickViewForWC: remove_add_to_cart_from_loop executed');
         remove_action('woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10);
-        // remove_action('woocommerce_after_shop_loop_item', 'woocommerce_loop_add_to_cart_link', 20);
-    }
-
-    public static function remove_default_product_link(): void
-    {
-        // remove_action('woocommerce_before_shop_loop_item', 'woocommerce_template_loop_product_link_open', 10);
-        // remove_action('woocommerce_after_shop_loop_item', 'woocommerce_template_loop_product_link_close', 5);
     }
 
     public static function custom_product_wrapper_open(): void
